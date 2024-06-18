6ee31440909c044565319e1a9d0ccf62ebcff769
David Hyman <5198267+davidhyman@users.noreply.github.com>
2024-06-18 20:56:34 +0100

    py3 + tweaks

--- watch_for_backup.py
+++ watch_for_backup.py
@@ -4,51 +4,62 @@ import shutil
 import os
 import time
 
-# hacky script for backing up a file
-# file, output dir, checking interval
+from pathlib import Path
+
+# hacky script for backing up a file;
+# provide commandline args as:
+# file, output dir, [checking interval in ms, 500]
+
+# n.b. should use inode watchers rather than polling ...
 
 args = sys.argv
 
-target = args[1]
+target = Path(args[1])
 
-target_filename =  os.path.split(target)[1]
+target_filename = target.name
 
-output = args[2]
+output = Path(args[2])
 
-output_format = "%s.%s"
+interval_ms = int(args[3]) if len(args) > 3 else 500
+interval = interval_ms / 1000
 
-interval = int(args[3]) if len(args) > 3 else 1
+print(f"will backup {target} to {output/target_filename} every {interval} seconds")
+
 
 def get_hash():
     hasher = hashlib.sha1()
-    with open(target, 'rb') as fh:
-        hasher.update(fh.read())
+    content = target.read_bytes()
+    hasher.update(content)
     return hasher.hexdigest()
-    
-def backup():
-    dest = os.path.join(output, output_format % (int(time.time()), target_filename))
-    shutil.copyfile(target, dest)
-    print 'backup: %s %s' % (time.strftime('%H:%M:%S'), dest)
 
-if not os.path.exists(target):
-    print 'file does not exist yet, waiting ...'
-while not os.path.exists(target):
-    time.sleep(interval)
-    
+
+def backup():
+    dest = output / f"{int(time.time())}.{target_filename}"
+    shutil.copyfile(target, dest)
+    print('backup: %s %s' % (time.strftime('%H:%M:%S'), dest))
+
+
+_state = None
 h_old = None
 t_old = None
 while True:
-    # check timestamps (cheap)
-    t_new = os.path.getmtime(target)
-    if t_new != t_old:
-        t_old = t_new
-                
-        # check hash (less cheap)
-        h_new = get_hash()
-        if h_new != h_old:
-            backup()
-            h_old = h_new
-        else:
-            print 'timestamp change, no backup: %s' % t_new
-            
+    try:
+        # check timestamps (cheap)
+        t_new = os.path.getmtime(target)
+        _state = True
+        if t_new != t_old:
+            t_old = t_new
+
+            # check hash (less cheap)
+            h_new = get_hash()
+            if h_new != h_old:
+                backup()
+                h_old = h_new
+            else:
+                print('timestamp change, no backup: %s' % t_new)
+
+    except FileNotFoundError:
+        if _state is not False: print('file does not exist yet, waiting ...')
+        _state = False
+
     time.sleep(interval)
